ae982c91ed6a4d3e629ecdb60036449dcaafabce
promehedi <admin@promehedi.com>
2022-10-31 16:03:47 +0600

    Display Products in shop page

--- market.py
+++ market.py
@@ -10,7 +10,21 @@ def home_page():
 
 @app.route('/shop')
 def shop_page():
-    return render_template('shop.html')
+    products = [
+        {'id': 1, 'name': 'Amazon Echo Dot 3rd Generation', 'price': 29.99,
+            'image': 'https://pm-proshop.herokuapp.com/images/alexa.jpg'},
+        {'id': 2, 'name': 'Cannon EOS 80D DSLR Camera', 'price': 929.99,
+            'image': 'https://pm-proshop.herokuapp.com/images/camera.jpg'},
+        {'id': 3, 'name': 'Airpods Wireless Bluetooth Headphones', 'price': 159.99,
+            'image': 'https://pm-proshop.herokuapp.com/images/airpods.jpg'},
+        {'id': 4, 'name': 'Sony Playstation 4 Pro White Version', 'price': 399.99,
+            'image': 'https://pm-proshop.herokuapp.com/images/playstation.jpg'},
+        {'id': 5, 'name': 'Logitech Gaming Mouse', 'price': 49.99,
+            'image': 'https://pm-proshop.herokuapp.com/images/mouse.jpg'},
+        {'id': 6, 'name': 'Apple iPhone 11 Pro Max', 'price': 999.99,
+            'image': 'https://pm-proshop.herokuapp.com/images/phone.jpg'},
+    ]
+    return render_template('shop.html', products=products)
 
 
 @app.route('/user/<name>')
