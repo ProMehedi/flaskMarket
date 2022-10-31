f1d5806a27fff70dda48842dba18631380a6140e
promehedi <admin@promehedi.com>
2022-10-31 15:43:02 +0600

    Change to Dark Mode

--- market.py
+++ market.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def home():
+def home_page():
     return render_template('home.html')
 
 
 @app.route('/shop')
-def shop():
+def shop_page():
     return render_template('shop.html')
 
 
 @app.route('/user/<name>')
-def user(name):
+def user_page(name):
     return f'<h1>Hey {name}, Welcome Back!</h1>'
