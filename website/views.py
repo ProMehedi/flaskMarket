c81687412805df97451da77d526d8f0be05ce468
promehedi <admin@promehedi.com>
2022-11-01 01:36:49 +0600

    Create All Pages & Route

--- website/views.py
+++ website/views.py
@@ -7,3 +7,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+
+@views.route('/products')
+def products():
+    return render_template("products.html")
