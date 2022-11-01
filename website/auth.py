b357763f5906d358adc4c8ecf42ce97531ac604f
promehedi <admin@promehedi.com>
2022-11-01 14:18:48 +0600

    Get register data & validate

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +13,23 @@ def logout():
     return render_template("logout.html")
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password')
+        password2 = request.form.get('cpassword')
+
+        if len(name) < 2:
+            flash('Name must be greater than 1 character.', category='danger')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='danger')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='danger')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='danger')
+        else:
+            flash('Account created!', category='success')
+
     return render_template("register.html")
