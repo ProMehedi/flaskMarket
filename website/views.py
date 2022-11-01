2e445bb79e39021cc09481a49ebbf7f88146b5a1
promehedi <admin@promehedi.com>
2022-11-01 22:16:55 +0600

    Add Products & Display

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, flash, render_template, request
+from flask_login import current_user, login_required
+from .models import Product
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -12,7 +14,8 @@ def home():
 
 @views.route('/products/')
 def products():
-    return render_template("products.html")
+    products = Product.query.all()
+    return render_template("products.html", products=products)
 
 
 @views.route('/admin/')
@@ -27,7 +30,22 @@ def admin_products():
     return render_template("admin/products.html")
 
 
-@views.route('admin/products/new/')
+@views.route('admin/products/new/', methods=['GET', 'POST'])
 @login_required
 def new_product():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        description = request.form.get('description')
+        image = request.form.get('image')
+        category = request.form.get('category')
+        price = request.form.get('price')
+        stock = request.form.get('stock')
+        rating = request.form.get('rating')
+
+        new_product = Product(owner=current_user.id, name=name, description=description,
+                              image=image, category=category, price=price, stock=stock, rating=rating)
+        db.session.add(new_product)
+        db.session.commit()
+        flash('Product added successfully!', category='success')
+
     return render_template("admin/new-product.html")
