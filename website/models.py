67105dd1b9c1a9babeed3ce52e9a4980153068c2
promehedi <admin@promehedi.com>
2022-11-01 14:34:26 +0600

    Create Product Model

--- website/models.py
+++ website/models.py
@@ -7,3 +7,17 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(55))
+    products = db.relationship('Product', backref='owner')
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    price = db.Column(db.Float)
+    description = db.Column(db.String(3200))
+    image = db.Column(db.String(255))
+    category = db.Column(db.String(150))
+    stock = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
