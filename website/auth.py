2e7073ca84bf5f20bd534ec937e51d59b0a27ca2
promehedi <admin@promehedi.com>
2022-11-01 15:07:49 +0600

    Login user

--- website/auth.py
+++ website/auth.py
@@ -8,6 +8,20 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='danger')
+        else:
+            flash('No account exist with that email.', category='danger')
+
     return render_template("login.html")
 
 
